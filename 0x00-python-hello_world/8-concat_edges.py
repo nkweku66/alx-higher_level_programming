b05597e7e07ace9ed322bff6cab85b35e3b55158
nkweku66 <nkweku66@gmail.com>
2024-07-20 09:53:54 -0700

    modify code structure

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 str = ("Python is an interpreted, interactive, object-oriented programming"
         " language that combines remarkable power with very clear syntax")
-str = str.split(" ")[5:7]
-str = " ".join(str) + " with Python"
+str = str.split(" ")[5:7] + str.split(" ")[12:13] + str.split(" ")[:1]
+str = " ".join(str)
 print(str)
