51c93272de5d4cfb41870101b298a976d7f93009
nkweku66 <nkweku66@gmail.com>
2024-07-20 07:25:15 -0700

    quick fix

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -2,5 +2,4 @@
 str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
 str = str.split(" ")
 str = str[5:8]
-str = " ".join(str) + " with python"
-print(str)
+print(" ".join(str) + " with python")
