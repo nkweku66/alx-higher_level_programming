1757bafc32b7d04640d0603cce7d55ec67033bb5
nkweku66 <nkweku66@gmail.com>
2024-07-21 05:34:32 -0700

    change the return method

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,10 +5,11 @@ if number > 0:
     last_digit = number % 10
 else:
     last_digit = (number * -1) % 10
+    last_digit = (last_digit * -1)
 
 if last_digit > 5:
-    print(f"Last digit of {number} is {last_digit} and is greater than 5")
+    return(f"Last digit of {number} is {last_digit} and is greater than 5")
 elif last_number < 6 and last_number != 0:
-    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
+    return(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
 else:
-    print(f"Last digit of {number) is {last_digit} and is 0")
+    return(f"Last digit of {number) is {last_digit} and is 0")
